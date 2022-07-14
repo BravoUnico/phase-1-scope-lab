5e6b35c146a72593d3df15d2e8b7c0389abbe49e
Alejandro <sancha0701@gmail.com>
2022-07-14 18:32:40 -0400

    commit tree 5773bc0d93707d9c1d3d4ad816bedbd87b4e9a3e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); // function that accesses that global customerName variable, and uppercases it.   
+}
+function setBestCustomer() {
+bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    bestCustomer ='maybe bob'
+}
+const leastFavoriteCustomer = "Pedro";
+function changeLeastFavoriteCustomer() {
+ leastFavoriteCustomer = "Peter"; 
+}
